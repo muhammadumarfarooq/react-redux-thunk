5a50848dd4cfe24cafc629d99f3e6aeb9bc8353f
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-01-11 15:12:47 +0500

    realizing the use of custom middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,15 +3,10 @@ import jsonplaceholder from "../apis/jsonPlaceholder";
 // http://jsonplaceholder.typicode.com/posts
 // http://jsonplaceholder.typicode.com/users
 
-export const fetchPosts = () => {
-  jsonplaceholder
-    .get("/posts")
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+export const fetchPosts = async () => {
+  const response = await jsonplaceholder.get("/posts");
+
+  console.log(response.data);
 
   return {
     type: "FETCH_POSTS"
