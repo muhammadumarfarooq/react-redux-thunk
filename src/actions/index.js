5d64a0e491f5b57a237a258489b49fb5915f3d5f
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-01-11 22:14:01 +0500

    Async action creator shorthand syntax

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,13 +3,11 @@ import jsonplaceholder from "../apis/jsonPlaceholder";
 // http://jsonplaceholder.typicode.com/posts
 // http://jsonplaceholder.typicode.com/users
 
-export const fetchPosts = () => {
-  return function(dispatch, getState) {
-    const response = jsonplaceholder.get("/posts");
+export const fetchPosts = () => dispatch => {
+  const response = jsonplaceholder.get("/posts");
 
-    dispatch({
-      type: "FETCH_POSTS",
-      payload: response
-    });
-  };
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response
+  });
 };
