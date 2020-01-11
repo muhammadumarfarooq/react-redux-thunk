f58edbb97ab88d4032fc5dc035d787aa12787084
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-01-11 22:22:44 +0500

    separate file for each reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from "redux";
+import postReducer from "./postsReducer";
 
-export default combineReducers({ dummy: () => "hi there!" });
+export default combineReducers({ posts: postReducer });
