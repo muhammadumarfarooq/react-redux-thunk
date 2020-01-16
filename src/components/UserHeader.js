bb6c07da22463f71cb1f675a9ca6cd5dd4913e0e
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-01-16 17:59:32 +0500

    user name showed in component

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -2,14 +2,16 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchUser } from "../actions";
 
-const UserHeader = ({ userId, fetchUser, user: users }) => {
+const UserHeader = ({ userId, fetchUser, users }) => {
   useEffect(() => {
     fetchUser(userId);
-
     //eslint-disable-next-line
   }, []);
 
-  return users !== null ? <h2>{/* {user.name} {userId} */}</h2> : `...`;
+  const user = users.find(user => user.id === userId);
+  if (!user) return null;
+
+  return <h6>{user.name}</h6>;
 };
 
 const mapStateToProps = state => {
