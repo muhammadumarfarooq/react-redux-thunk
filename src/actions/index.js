916888094a667ec52f5267b52fbcde0580591194
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-01-16 18:45:46 +0500

    refactor to switch statement

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,5 @@
 import jsonplaceholder from "../apis/jsonPlaceholder";
 
-// http://jsonplaceholder.typicode.com/posts
-// http://jsonplaceholder.typicode.com/users
-
 export const fetchPosts = () => async dispatch => {
   const response = await jsonplaceholder.get("/posts");
 
